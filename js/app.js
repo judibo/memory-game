ab3f5d4fcc4a075a319f7f541bb3bd6fa72d25cb
Julia Dibo <judibo@Julias-MacBook-Air.local>
2018-08-14 01:07:11 -0300

    created logic game

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@
  */
 
 const deck = document.querySelector('.deck');
+let moves = 0;
+let toggledCards = [];
+let clockOff = true;
+let time = 0;
+let clockId;
+let matched = 0;
+const pairs = 8;
 
 /*
  * Display the cards on the page
@@ -26,23 +33,44 @@ function shuffle(array) {
     return array;
 }
 
-let toggledCards = [];
+function shuffleDeck() {
+	const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
+	const shuffledCards = shuffle(cardsToShuffle);
+	for (card of shuffledCards) {
+		card.classList.remove('show');
+		card.classList.remove('open');
+		card.classList.remove('match')
+		deck.appendChild(card);
+	}
+}
+shuffleDeck();
+
+function isValid(clickTarget) {
+	return (clickTarget.classList.contains('card') && !clickTarget.classList.contains('match') && toggledCards.length < 2 && !toggledCards.includes(clickTarget));
+}
 
 deck.addEventListener('click', event => {
 	const clickTarget = event.target;
-	if (clickTarget.classList.contains('card') && toggledCards.length < 2) {
+	if (isValid(clickTarget)) {
+		if(clockOff) {
+			startClock();
+			clockOff = false;
+		}
 		toggleCard(clickTarget);
 		addToggleCard(clickTarget);
+		addMove();
+		checkScore();
 		if (toggledCards.length === 2) {
-			console.log('2 cards!');
+			checkIfMatch(clickTarget);
 		}
+
 	}
 });
 
 // Display the card's symbol when clicked
-function toggleCard(clickTarget) {
-	clickTarget.classList.toggle('open');
-	clickTarget.classList.toggle('show');
+function toggleCard(card) {
+	card.classList.toggle('open');
+	card.classList.toggle('show');
 }
 
 // Add the card to a *list* of "open" cards
@@ -51,13 +79,162 @@ function addToggleCard(clickTarget) {
 	console.log(toggledCards);
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//See if cards match
+function checkIfMatch() {
+	if (
+		toggledCards[0].firstElementChild.className === toggledCards[1].firstElementChild.className ) {
+		toggledCards[0].classList.toggle('match');
+		toggledCards[1].classList.toggle('match');
+		toggledCards = [];
+		matched++;
+	} else {
+		setTimeout(() => {
+			toggleCard(toggledCards[0]);
+			toggleCard(toggledCards[1]);
+			toggledCards = [];
+		}, 1000);
+}
+}
+
+//add moves
+function addMove() {
+	moves++;
+	const movesText = document.querySelector('.moves');
+	movesText.innerHTML = moves;
+}
+
+//stars bases on move count
+function checkScore() {
+	if (moves === 14 || moves === 20) {
+		hideStar();
+	}
+}
+
+function hideStar () {
+	const starList = document.querySelectorAll('.stars li');
+	for (star of starList) {
+		if (star.style.display !== 'none') {
+			star.style.display = 'none';
+			break;
+		}
+	}
+}
+
+//timer starts
+function startClock () {
+	clockId = setInterval(() => {
+		time++;
+		displayTime();
+	}, 1000);
+}
+
+
+function displayTime() {
+	const clock = document.querySelector('.clock');
+	const minutes = Math.floor(time / 60);
+	const seconds = time % 60;
+	clock.innerHTML = time;
+	if (seconds < 10) {
+		clock.innerHTML = `${minutes}:0${seconds}`;
+	} else {
+		clock.innerHTML = `${minutes}:${seconds}`;
+	}
+}
+
+//timer stops
+function stopClock() {
+	clearInterval(clockId);
+}
+
+//modal with stats
+function toggleModal() {
+	const modal = document.querySelector('.modal__background');
+	modal.classList.toggle('hide');
+}
+
+
+function writeModalStats() {
+	const timeStat = document.querySelector('.modal__time');
+	const clockTime = document.querySelector('.clock').innerHTML;
+	const movesStat = document.querySelector('.modal__moves');
+	const starsStat = document.querySelector('.modal__stars');
+	const stars = getStars();
+	timeStat.innerHTML= `Time = ${clockTime}`;
+	movesStat.innerHTML = `Moves = ${moves}`;
+	starsStat.innerHTML = `Stars = ${stars}`;
+}
+
+function getStars() {
+	stars = document.querySelectorAll('.stars li');
+	starCount = 0;
+	for (star of stars) {
+		if (star.style.display !== 'none') {
+			starCount++;
+		}
+	}
+	console.log(starCount);
+	return starCount;
+}
+
+
+document.querySelector('.modal__cancel').addEventListener('click', toggleModal);
+document.querySelector('.restart').addEventListener('click', resetGame);
+document.querySelector('.modal__replay').addEventListener('click', replayGame);
+
+
+
+function resetGame() {
+	resetClockAndTime();
+	resetMoves();
+	resetStars();
+	resetCards();
+	shuffleDeck();
+
+}
+
+function resetClockAndTime() {
+	stopClock();
+	clockOff = true;
+	time = 0;
+	displayTime();
+}
+
+function resetMoves() {
+	moves = 0;
+	document.querySelector('.moves').innerHTML = moves;
+}
+
+function resetStars() {
+	stars = 0;
+	const starList = document.querySelector('.star li');
+	for (star of starsList) {
+		star.style.display = 'inline';
+	}
+}
+
+function resetCards() {
+	const cards = document.querySelectorAll('.deck li');
+	for (let card of cards) {
+		card.className = 'card';
+	}
+
+}
+
+function replayGame() {
+	resetGame();
+	toggleModal();
+}
+
+
+function checkWin() {
+	if (matched === pairs) {
+		gameOver();
+	}
+}
+
+function gameOver() {
+	stopClock();
+	writeModalStats();
+	toggleModal();
+}
+
